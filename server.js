9d0881c8d87907593b4240fcbcfad64068be699a
Lcole.engr <lcole.engr@gmail.com>
2019-12-03 09:05:56 -0500

    orm.js file completed

--- server.js
+++ server.js
@@ -33,3 +33,22 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
