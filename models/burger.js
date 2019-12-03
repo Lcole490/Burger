92706f9ce83058229656a170b64d21cb6f2e1175
Lcole.engr <lcole.engr@gmail.com>
2019-12-03 09:12:16 -0500

    updated burger.js with orm requires

--- models/burger.js
+++ models/burger.js
@@ -5,5 +5,23 @@ var burger = {
         orm.selectAll("burgers", function(res){
             cb(res);
         });
+    },
+
+    insertOne: function(cols, vals, cb) {
+        orm.selectAll("burgers", cols, vals, function(res){
+            cb(res);
+        });
+    },
+
+    updateOne: function(objColVals, condition, cb) {
+        orm.selectAll("burgers", objColVals, condition, function(res){
+            cb(res);
+        });
+    },
+
+    deletOne: function(condition, cb) {
+        orm.selectAll("burgers", condition, function(res){
+            cb(res);
+        });
     }
 }
