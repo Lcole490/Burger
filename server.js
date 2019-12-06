800beb3b953a6aa8c31a77f5cdd927ecd07b1a2b
Lcole.engr <lcole.engr@gmail.com>
2019-12-06 11:33:48 -0500

    included routes from burger controller in file

--- server.js
+++ server.js
@@ -22,31 +22,26 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "root",
+//   database: "burgers_db"
+// });
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 
+var routes = require("./controllers/burgers_controller.js");
 
-
-
-
-
-
+app.use(routes);
 
 
 
