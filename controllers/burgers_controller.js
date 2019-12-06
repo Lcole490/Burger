774d5f20b78f047030098550186401c630c44431
Lcole.engr <lcole.engr@gmail.com>
2019-12-06 11:39:24 -0500

    fixed delete function

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -51,7 +51,7 @@ router.put("/api/burgers/:id", function(req, res){
 });
 
 
-router.deleteOne("/api/burgers/:id", function(req, res){
+router.delete("/api/burgers/:id", function(req, res){
     var condition = " id = " + req.params.id;
     console.log("condition", condition);
 
